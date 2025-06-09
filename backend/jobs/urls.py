120998f95f00d9b9b6385518318ba7cc999c854b
Rajankumar80 <rajankumargupta254@gmail.com>
2025-06-09 18:04:37 +0000

    Added Rest Framework to backend, now api is working

--- backend/jobs/urls.py
+++ backend/jobs/urls.py
@@ -1,7 +1,50 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 
-urlpatterns = [
-    path('', views.index, name='index'),
+from rest_framework import routers, serializers, viewsets
+from .models import Company, Job, Tag
+
+class CompanySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Company
+        fields = '__all__'
+
+class TagSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Tag
+        fields = '__all__'
+
+class JobSerializer(serializers.ModelSerializer):
+    company = CompanySerializer(read_only=True)
+    tags = TagSerializer(many=True, read_only=True)
     
+    class Meta:
+        model = Job
+        fields = '__all__'
+
+class CompanyViewSet(viewsets.ModelViewSet):
+    queryset = Company.objects.all()
+    serializer_class = CompanySerializer
+
+class TagViewSet(viewsets.ModelViewSet):
+    queryset = Tag.objects.all()
+    serializer_class = TagSerializer
+
+class JobViewSet(viewsets.ModelViewSet):
+    queryset = Job.objects.all()
+    serializer_class = JobSerializer
+
+
+router = routers.DefaultRouter()
+router.register(r'companies', CompanyViewSet)
+router.register(r'tags', TagViewSet)
+router.register(r'jobs', JobViewSet)
+router.register(r'users', views.UserViewSet)
+router.register(r'groups', views.GroupViewSet)
+
+
+urlpatterns = [
+    path('', include(router.urls)),
+        path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+
 ]
