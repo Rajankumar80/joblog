592de80a235d12595e27cb25683fc6d411f6a7a7
Rajankumar80 <rajankumargupta254@gmail.com>
2025-06-09 19:32:51 +0000

    Added filtering

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -57,7 +57,11 @@ REST_FRAMEWORK = {
     
         'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
-    ]
+    ],
+            'DEFAULT_FILTER_BACKENDS': [
+        'django_filters.rest_framework.DjangoFilterBackend',
+        'rest_framework.filters.OrderingFilter',
+    ],
 }
 
 
